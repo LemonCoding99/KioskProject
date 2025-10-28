a54c0860d40d3f9cb4234a70b0bec9aace27a2c0
LemonCoding99 <qordmstj7530@gmail.com>
2025-10-28 17:26:05 +0900

    refactor(Lv5): Refactor prerequisite tasks before starting the challenge (도전과제 시작 전 필수과제 로직 리팩토링)

--- src/kioskchallenge/Kiosk.java
+++ src/kioskchallenge/Kiosk.java
@@ -15,8 +15,36 @@ public class Kiosk {
     }
 
     // 3. 기능
+    // start(): 메뉴출력, 사용자 입력값에 따른 매뉴 출력 및 종료
+    public void start() {
+        Scanner scanner = new Scanner(System.in); // 스캐너 선언
+        boolean startExit = true; // 메인루프 구분: 0이 입력되면 반복문 종료
 
-    // 카테고리 메뉴 출력하는 함수
+        while (startExit) {
+            printMenu(); // MAIN MENU 출력하기
+            int selectedNumber = handleCategoryMenu(scanner);  // handleCategoryMenu 메서드 사용해 입력받은 값 가져오기
+
+            // 문자 입력(return값 -1)일 경우 건너뛰기
+            if (selectedNumber == -1) {
+                continue;  // 쓰지 않을 경우 경고문구가 2가지 모두 출력됨!
+            }
+
+            // 카테고리 메뉴 0인 경우 프로그램 종료하기
+            if (selectedNumber == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                startExit = false;
+            // 입력받은 카테고리 메뉴 출력하기
+            } else if (0 < selectedNumber && selectedNumber <= categoryList.size()) {
+                Menu selectedCategory = categoryList.get(selectedNumber -1);
+                handleMenuItem(scanner, selectedCategory); // handleMenuItem 메서드 사용해 세부메뉴 값 가져오기
+            // 번호가 잘못 입력된 경우 경고문구 출력하기
+            } else {
+                System.out.println("번호가 잘못 입력되었습니다. 다시 입력해주세요.");
+            }
+        }
+    }
+
+    // Category 메뉴 출력하는 메서드
     public void printMenu() {
         System.out.println("[ MAIN MENU ]");
         for(int i = 0; i < categoryList.size(); i++) {
@@ -25,56 +53,38 @@ public class Kiosk {
         System.out.println(0 + ". 종료    | 종료하기");  // 0입력시 종료
     }
 
-    // start: 메뉴출력, 사용자 입력값에 따른 매뉴 출력 및 종료
-    public void start() {
-        Scanner scanner = new Scanner(System.in); // 스캐너 선언
+    // Category 메뉴 입력 및 예외처리 메서드
+    private int handleCategoryMenu(Scanner scanner) {
+        try {
+            return scanner.nextInt();
+        } catch (Exception e) {  // 예외처리: 문자열 입력시 경고문구 출력
+            System.out.println("메뉴 번호만 입력하세요.");
+            scanner.nextLine(); // 개행문자 제거
+            return -1;  // 입력오류 발생한 경우
+        }
+    }
 
-        // 카테고리 메뉴 반복문
-        boolean startExit = true; // 메인루프 구분: 0이 입력되면 반복문 종료
-        while (startExit) {
-            printMenu(); // MAIN MENU 출력하기
-
-            try {
-                int selectedNumber = scanner.nextInt();
-                scanner.nextLine();  // 개행문자 제거하기 -> 무한반복 오류 발생할 수 있음
-
-                // 입력받은 숫자 확인하기
-                if (selectedNumber == 0) {
-                    System.out.println("프로그램을 종료합니다.");
-                    startExit = false;
-
-                } else if ((0 < selectedNumber) && selectedNumber <= categoryList.size()) {
-                    // 선택한 카테고리 메뉴 리스트 가져오기
-                    Menu selectedCategory = categoryList.get(selectedNumber - 1);
-                    selectedCategory.printMenuItems(); // 선택된 상세 메뉴판 출력하는 함수
-
-                    // 상세메뉴 반복문
-                    boolean menuExit = true;  // 서브루프 구분: 0이 입력되면 돌아가기
-                    while (menuExit) {
-                        try {
-                            selectedNumber = scanner.nextInt(); // 메뉴 입력받기
-                            scanner.nextLine(); // 개행문자 제거하기
-
-                            if (selectedNumber == 0) {
-                                System.out.println("뒤로가기"); // 0일 경우 하위 메뉴 종료하고 카테고리로 돌아가기
-                                menuExit = false;
-
-                            } else if ((0 < selectedNumber) && selectedNumber <= selectedCategory.menuItems.size()) {
-                                MenuItem selectedItem = selectedCategory.getMenuItem(selectedNumber - 1);  // 선택한 메뉴 객체 출력하기
-                                System.out.println("선택한 메뉴: " + selectedItem.getName() + "    | W " + selectedItem.getPrice() + " | " + selectedItem.getInfo());
-                            } else {
-                                System.out.println("번호가 잘못 입력되었습니다. 다시 입력해주세요.");  // 번호가 잘못 입력된 경우
-                            }
-                        } catch (Exception e) {
-                            System.out.println("메뉴 번호만 입력하세요.");  // 숫자외의 것이 입력된 경우
-                            scanner.nextLine();
-                        }
-                    }
-                } else {
-                    System.out.println("번호가 잘못 입력되었습니다. 다시 입력해주세요.");  // 번호가 잘못 입력된 경우
-                }
-            } catch (Exception e) {
-                System.out.println("메뉴 번호만 입력하세요.");  // 숫자외의 것이 입력된 경우
+    // MenuItems 메뉴 입력받은 숫자 판별 메서드
+    private void handleMenuItem(Scanner scanner, Menu selectedCategory) {
+        boolean menuExit = true;
+        selectedCategory.printMenuItems(); // 선택된 MenuItems 메뉴판 출력
+        while (menuExit) {
+            int selectedNumber = handleCategoryMenu(scanner);  // 세부메뉴 입력처리
+            // 문자 입력(return값 -1)일 경우 건너뛰기
+            if (selectedNumber == -1) {
+                continue;
+            }
+            // 0이 입력된 경우 카테고리별 상세 메뉴 종료하고 카테고리 메뉴로 돌아가기
+            if (selectedNumber == 0) {
+                System.out.println("뒤로가기");
+                menuExit = false;
+            // 입력된 카테고리별 상세 메뉴 가져와서 출력하기
+            } else if ((0 < selectedNumber) && selectedNumber <= selectedCategory.menuItems.size()) {
+                MenuItem selectedItem = selectedCategory.getMenuItem(selectedNumber - 1);  // 선택한 메뉴 객체 출력하기
+                System.out.println("선택한 메뉴: " + selectedItem.getName() + "    | W " + selectedItem.getPrice() + " | " + selectedItem.getInfo());
+            // 번호가 잘못 입력된 경우 경고문구 출력하기
+            } else {
+                System.out.println("번호가 잘못 입력되었습니다. 다시 입력해주세요.");  // 번호가 잘못 입력된 경우
             }
         }
     }
--- src/kioskchallenge/MenuItem.java
+++ src/kioskchallenge/MenuItem.java
@@ -1,6 +1,6 @@
 package kioskchallenge;
 
-class MenuItem {
+class   MenuItem {
     // 햄버거의 이름, 가격 설명
     // 1. 속성
     private final String name;
